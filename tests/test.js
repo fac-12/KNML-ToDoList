0375300044da177876962079f19dcf7f84b0f63e
trc5019 <trc5019@gmail.com>
2017-11-01 14:05:40 +0000

    written first failing test

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,20 @@
 var test = require('tape');
-var logic = require('./logic');
+var logic = require('../logic.js');
 
-test('Example test', function(t) {
-  t.pass();
+  var state = [
+    { id: -3, description: 'first todo' },
+    { id: -2, description: 'second todo' },
+    { id: -1, description: 'third todo' },
+  ];
+
+
+
+test('output is an array', function(t) {
+  t.equal(typeof logic.deleteTodo(state, "-2"), Array, "typeof output should be an array");
   t.end();
 });
+
+// test('array length is one less than input length', function(t) {
+//   t.equal(logic.deleteTodo(todos, idToDelete).length, logic.todos.length - 1, "length of array should be one less than input length");
+//   t.end();
+// });
