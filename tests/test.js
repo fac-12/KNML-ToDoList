fd68861b6601055ef32ab08a44dec390ca6e31ac
trc5019 <trc5019@gmail.com>
2017-11-01 14:52:36 +0000

    third passing test

--- tests/test.js
+++ tests/test.js
@@ -7,7 +7,14 @@ var logic = require('../logic.js');
     { id: -1, description: 'third todo' },
   ];
 
-
+test('input array has not been altered', function(t) {
+    t.deepEqual(state, [
+      { id: -3, description: 'first todo' },
+      { id: -2, description: 'second todo' },
+      { id: -1, description: 'third todo' },
+    ], "input array should not be altered");
+    t.end();
+});
 
 test('output is an array', function(t) {
   t.equal(Array.isArray(logic.deleteTodo(state, -2)), true, "typeof output should be an array");
